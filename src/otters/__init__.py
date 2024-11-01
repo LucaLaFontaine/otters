39333c534ebee1996256aa91a8a7198cdb53986a
lucalafontaine <llafontaine@akonovia.ca>
2024-11-01 13:02:02 -0400

    addding formatData to the import all

--- src/otters/__init__.py
+++ src/otters/__init__.py
@@ -1,8 +1,8 @@
 
 # Ok so if you import a module into __init__.py you can then use "import *"
 #   so we'll import all the like basic commodities in here even though PEP something doesn't like this
-from .wrangle.file_loader import import_config
+from .wrangle.file_loader import import_config, formatData
 from .wrangle.time_tools import str2dt
-__all__ = ["drive", "model", "vis", "import_config", 'str2dt', 'wrangle']
+__all__ = ["drive", "model", "vis", 'wrangle', "import_config", 'str2dt', 'formatData']
 
 __pdoc__ = {}
